887a428bcb5e3485180f64ddaace4e3d655dd5fd
slsusperrabbit <slsusperrabbit@gmail.com>
2017-06-07 12:40:38 -0400

    test scale

--- examples/user_code/usr_openpose.cpp
+++ examples/user_code/usr_openpose.cpp
@@ -70,6 +70,8 @@ int openPoseTutorialPose1()
     const auto numberPeople = poseKeyPoints.getSize(0);
     const auto numberBodyParts = poseKeyPoints.getSize(1);
     
+    float scale = 1.0/scaleInputToOutput;
+    
     //generate json object
     std::string res_json = "";
 
@@ -81,9 +83,9 @@ int openPoseTutorialPose1()
 		res_json +=  "\"joints\":";
 		res_json +=  "[";
 		for (int ij=0;ij<numberBodyParts;ij++) {
-			res_json += std::to_string(poseKeyPoints[ip*numberBodyParts*3 + ij*3+0]);
+			res_json += std::to_string(scale*poseKeyPoints[ip*numberBodyParts*3 + ij*3+0]);
 			res_json += ",";
-			res_json += std::to_string(poseKeyPoints[ip*numberBodyParts*3 + ij*3+1]);
+			res_json += std::to_string(scale*poseKeyPoints[ip*numberBodyParts*3 + ij*3+1]);
 			res_json += ",";
 			res_json += std::to_string(poseKeyPoints[ip*numberBodyParts*3 + ij*3+2]);
 			if (ij<numberBodyParts-1) res_json += ",";
