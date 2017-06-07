50487ffd09ee4317000b366664cd41ad45a8921a
slsusperrabbit <slsusperrabbit@gmail.com>
2017-06-07 18:13:59 -0400

    init test

--- examples/user_code/ylwrapper_openpose.cpp
+++ examples/user_code/ylwrapper_openpose.cpp
@@ -11,12 +11,11 @@ int main()
 	
 	YLWrapper ylw;
 	
-	std::cout << "init ready! \n";
+	//std::cout << "init ready! \n";
 	
 	const auto timerBegin = std::chrono::high_resolution_clock::now();
 	
-	for(int i=0; i<10; i++) {
-		//std::cout << "index: " << i << "\n";
+	for(int i=0; i<100; i++) {
 		std::string res_json = ylw.getPoseEstimation(img_mat);
 	
 		std::cout << "index: " << i << "\n";
@@ -24,8 +23,27 @@ int main()
 		std::cout << res_json << "\n";
 	}
 	
-	const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-timerBegin).count() * 1e-9;
+	//const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-timerBegin).count() * 1e-9;
 	
+	//std::cout << "Total time: " << std::to_string(totalTimeSec) << " seconds.";
+	
+	
+	cv::Mat img_mat2 = op::loadImage(image_path, CV_LOAD_IMAGE_COLOR);
+		
+	YLWrapper ylw2;
+	
+	for(int j=0; j<100; j++) {
+		std::string res_json2 = ylw2.getPoseEstimation(img_mat2);
+	
+		std::cout << "index: " << j << "\n";
+		std::cout << "JSON Result: \n";
+		std::cout << res_json2 << "\n";
+	}
+	
+	
+	const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-timerBegin).count() * 1e-9;
+		
 	std::cout << "Total time: " << std::to_string(totalTimeSec) << " seconds.";
+	
 	
 }
