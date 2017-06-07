4c9276c528c2f65a4b1b29a7041fd48351ca1914
slsusperrabbit <slsusperrabbit@gmail.com>
2017-06-07 14:27:44 -0400

    bug fix

--- src/openpose/wrapper/ylwrapper.cpp
+++ src/openpose/wrapper/ylwrapper.cpp
@@ -1,52 +1,6 @@
-#ifndef OPENPOSE__WRAPPER__YLWRAPPER_HPP
-#define OPENPOSE__WRAPPER__YLWRAPPER_HPP
 
+#include "openpose/wrapper/ylwrapper.hpp"
 
-#include <openpose/core/headers.hpp>
-#include <openpose/pose/headers.hpp>
-
-
-    class YLWrapper
-    {
-    public:
-        /**
-         * Constructor.
-         */
-        YLWrapper();
-
-        std::string getPoseEstimation(cv::Mat oriImg);
-        
-        void freeGPU();
-
-        
-
-    private:
-        cv::Size outputSize;
-		cv::Size netInputSize;
-		cv::Size netOutputSize;
-		op::PoseModel poseModel;
-		
-		int num_scales = 1;
-		float scale_gap = 0.3;
-		float alpha_pose = 0.6;
-		int num_gpu_start = 0;
-		std::string model_folder = "models/";
-		
-		op::CvMatToOpInput cvMatToOpInput;
-		op::CvMatToOpOutput cvMatToOpOutput;
-		op::PoseExtractorCaffe poseExtractorCaffe;
-		op::PoseRenderer poseRenderer;
-		
-    };
-
-
-
-
-
-/*
-// Implementation
-#include <openpose/core/headers.hpp>
-#include <openpose/pose/headers.hpp>
 
 	YLWrapper::YLWrapper() {
 		outputSize.width = 1280;
@@ -121,6 +75,4 @@
     	
     }
 	
-*/
 
-#endif // OPENPOSE__WRAPPER__YLWRAPPER_HPP
